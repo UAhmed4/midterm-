8c34d9f1f8c0d7feffec6703b014f2e28d12300c
UAhmed4 <159491064+UAhmed4@users.noreply.github.com>
2024-09-30 18:58:28 -0700

    Add files via upload

---
+++ midterm.bat
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>First Layout</title>
+    <style>
+        body {
+            background-color: black;
+            color: white; 
+        }
+        h1 {
+            color: red;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>Usman Ahmed Photography</h1>
+        <p>Photographs by Usman Ahmed</p>
+    </header>
+    
+    <h2>Image Archive</h2> 
+    <img src="DSC_0137.NEF" alt="Image 1" width="400" height="400">
+    <img src="DSC_0081.NEF" alt="Image 2" width="400" height="400">
+    <img src="DSC_0055.NEF" alt="Image 3" width="400" height="400">
+
+    <h3>2024</h3>
+    /images
+c:img<alt>\Users\LeBro\Desktop\2024 Photo Archive\DSC_0001.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0002.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0003.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0004.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0006.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0009.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0010.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0012.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0015.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0017.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0019.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0020.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0021.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0024.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0025.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0027.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0028.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0029.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0031.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0032.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0033.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0034.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0035.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0037.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0038.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0039.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0040.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0041.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0044.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0045.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0046.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0047.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0048.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0051.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0052.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0054.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0056.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0057.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0058.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0059.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0060.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0062.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0063.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0065.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0067.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0068.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0069.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0072.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0073.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0074.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0075.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0076.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0077.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0078.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0079.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0080.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0088.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0094.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0095.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0096.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0100.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0101.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0102.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0103.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0104.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0105.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0106.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0107.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0109.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0110.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0111.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0112.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0113.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0114.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0115.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0116.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0118.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0121.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0123.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0124.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0126.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0127.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0128.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0129.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0131.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0132.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0134.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0135.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0138.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0139.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0140.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0141.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0143.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0144.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0146.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0147.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0149.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0153.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0154.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0155.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0156.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0158.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0159.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0160.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0161.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0163.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0164.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0167.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0168.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0169.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0170.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0171.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0172.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0173.NEF c:\Users\LeBro\Desktop\2024 Photo Archive\DSC_0174.NEF
+</body>
+</html>
+
+body {
+    background-color: black;
+    color: white;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+
+h1 {
+    color: red;
+    width: 100%;
+    text-align: center;
+}
+
+img {
+    margin: 1px;
+    width: 200px; 
+    height: auto; 
+}
